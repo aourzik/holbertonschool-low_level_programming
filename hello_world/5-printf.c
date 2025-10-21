9f09d5ce83a7aa960cc37f869fb3b4df3f896ba4
aourzik <a.ourzik@gmail.com>
2025-10-21 09:29:24 +0200

    feat: create script to print a sentence

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with propergrammar,but the outcome is a piece of art,");
+    return (0);
+}
