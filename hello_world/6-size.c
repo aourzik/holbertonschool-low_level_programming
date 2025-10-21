fcea180ae8e96dc57982c4ef35f7d2172da98ba2
aourzik <a.ourzik@gmail.com>
2025-10-21 09:51:46 +0200

    feat: creat script to print the size of different types on computer

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char:", sizeof(char));
+	printf("Size of a an int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
+	printf("Size of a float:", sizeof(float));
+	return (0);
+}
