0afdf0ea21940c7bafaeb731e838f7c39effbb0b
aourzik <a.ourzik@gmail.com>
2025-10-22 10:52:57 +0200

    feat: create program that prints numbers of base ten

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
