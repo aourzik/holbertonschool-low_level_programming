33a12f2c27269ec82793e5d8305300d9bdc6d3e2
aourzik <a.ourzik@gmail.com>
2025-10-22 13:00:39 +0200

    feat: create program that prints all combinations of single digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		
+		if (i < '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+
+	putchar('\n');
+	return (0);
+}
