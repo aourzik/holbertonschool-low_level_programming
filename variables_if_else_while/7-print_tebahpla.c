3c404f0a18d019bf6e6a5bd4e8bae4e6d983c1f9
aourzik <a.ourzik@gmail.com>
2025-10-22 11:14:59 +0200

    feat: create program that prints alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
