21e549c7b77f53893688e41b83b5ec10ec4f30db
aourzik <a.ourzik@gmail.com>
2025-10-22 09:44:07 +0200

    feat: create program that prints alphabet lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
