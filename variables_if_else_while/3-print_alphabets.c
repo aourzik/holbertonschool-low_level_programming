6cb1c5120ed7723803eaa54e1bda05884ffbc103
aourzik <a.ourzik@gmail.com>
2025-10-22 10:25:35 +0200

    fix: fix space problem in indentation code

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 	char c = 'a';
-    char C = 'A';
+	char C = 'A';
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
 
-    for (C = 'A'; C <= 'Z'; C++)
+	for (C = 'A'; C <= 'Z'; C++)
 	{
 		putchar(C);
 	}
