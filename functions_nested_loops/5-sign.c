e5b6bfa964246dee8349f9ea7111a059c66dd3ef
aourzik <a.ourzik@gmail.com>
2025-10-24 11:27:09 +0200

    fix: use an other function in the program to be exercise friendly

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		write(1, '+', 1);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar('-');
+		write(1, '-', 1);
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		write(1, '0', 1);
 		return (0);
 	}
 }
