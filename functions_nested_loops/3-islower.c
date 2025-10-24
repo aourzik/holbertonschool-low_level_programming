b20b3be221b50390ded1774f90df36a53313d344
aourzik <a.ourzik@gmail.com>
2025-10-24 10:46:15 +0200

    fix modify two files who were not a the good place

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -6,16 +6,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
+	if ((c >= 'a' && c <='z') || (c >= '0' && c <= '9'))
+	return (1);
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-int _islower(int c)
+int main(void)
 {
-	if ((c >= 'a' && c <='z') || (c >= '0' && c <= '9'))
-	return (1);
+	int r;
 
-	else
-	{
-		return (0);
-	}
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
