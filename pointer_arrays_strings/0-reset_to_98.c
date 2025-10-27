338e6574440e84b503b8684de0a25fb426342849
aourzik <a.ourzik@gmail.com>
2025-10-27 15:34:58 +0100

    feat: create a function that takes a pointer to an int as parameter and updates the value

---
+++ pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of an integer to 98
+ * @n: pointer to the integer to be updated
+ *
+ * Description: This function takes a pointer to an integer as a parameter
+ *              and sets the value of the integer it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
