5c411a2efb60bbda20a44dd2ac66da603042395d
aourzik <a.ourzik@gmail.com>
2025-10-27 10:48:27 +0100

    feat: create a function that multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*mul - multiplie deux entiers
+*@a: premier entier
+*@b: deuxième entier
+*Description:
+*Cette fonction prend deux entiers en paramètres et renvoie
+*le résultat de leur multiplication.
+*Return: le produit de a et b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
