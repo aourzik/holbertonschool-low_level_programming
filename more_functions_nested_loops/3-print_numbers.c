f6ac0cf2eeaeec3dd5b4db9a270e651e9af1b2f2
aourzik <a.ourzik@gmail.com>
2025-10-27 11:03:42 +0100

    feat: create function that prints numbers followed by a new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - affiche les chiffres de 0 à 9 suivis d'une nouvelle ligne
+ *
+ * Return: rien (void)
+ */
+void print_numbers(void)
+{
+	int j;
+	char numbers[] = "0123456789$";
+
+		for (j = 0; numbers[j] != '\0'; j++)
+		{
+			_putchar(numbers[j]);
+		}
+		_putchar('\n');
+}
