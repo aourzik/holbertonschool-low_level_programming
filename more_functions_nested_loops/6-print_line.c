80dd5face258d72030d3f62e2cdc3e0681d8ab6a
aourzik <a.ourzik@gmail.com>
2025-10-27 13:21:53 +0100

    feat: create a function that draws a straight line in the terminal

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - dessine une ligne droite dans le terminal
+ * @n: nombre de fois que le caractère '_' doit être imprimé
+ *
+ * Description:
+ * Cette fonction utilise uniquement _putchar pour afficher le caractère '_'
+ * à l'écran n fois, suivi d'un saut de ligne. Si n est 0 ou moins, seule
+ * la nouvelle ligne est affichée.
+ *
+ * Return: rien (void)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
