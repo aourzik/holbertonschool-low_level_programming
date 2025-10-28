83120e35c7cb348f2a93b7350200315be63d3754
aourzik <a.ourzik@gmail.com>
2025-10-28 10:30:54 +0100

    fix: modify the function to print in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,16 @@
  */
 void print_rev(char *s)
 {
-	int i = 62;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		i++;
+	}
+	while (i > 0)
+	{
 		i--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
