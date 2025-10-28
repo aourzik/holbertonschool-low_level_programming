df6aad50548479e6037b97efb72448e33153b026
aourzik <a.ourzik@gmail.com>
2025-10-28 13:02:33 +0100

    feat: create function that prints every other characters of a string.

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ *         starting with the first character,
+ *         followed by a new line.
+ * @str: the string to print
+ *
+ * Description: This function loops through the string,
+ *              printing only characters at even indices
+ *              (0, 2, 4, ...). After printing all relevant
+ *              characters, it prints a newline.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
