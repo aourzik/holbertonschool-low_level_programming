54770f1327a2997761421338f7d11e2e5745966e
aourzik <a.ourzik@gmail.com>
2025-10-29 15:06:57 +0100

    fix: modify function to handle all conditions

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 < s2)
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		return (-15);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	else if (s1 > s2)
-	{
-		return (15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (s1[i] - s2[i]);
 }
