daa81fad1c12f8d28bd44a6586aa03d3664cd7f4
aourzik <a.ourzik@gmail.com>
2025-10-30 15:08:14 +0100

    feat: create a function that converts a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * This function parses a string, ignores any non-digit characters
+ * at the beginning, handles optional '+' and '-' signs, and
+ * converts the numeric portion into an integer.
+ *
+ * Return: the converted integer. If the number is negative,
+ * the result will be multiplied by -1 accordingly.
+ */
+int _atoi(char *s)
+{
+	int nb = 0;
+	int i = 0;
+	int sign = 1;
+	int value;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		value = s[i] - '0';
+		nb = nb * 10 + value;
+		i++;
+	}
+	return (nb * sign);
+}
