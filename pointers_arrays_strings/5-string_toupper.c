f20730c8e3d8b45b4a9b715caec02c2f7eb73d8a
aourzik <a.ourzik@gmail.com>
2025-10-30 08:59:25 +0100

    fix: modify comment to be betty friendly

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * string_toupper - Change toutes les lettres minusculres d'une chaine en majuscules.
- * @str: Pointeur vers la chaine de caractères à modifier.
+ * string_toupper - Change toutes les lettres minuscules d'une chaîne en majuscules.
+ * @str: Pointeur vers la chaîne de caractères à modifier.
  *
- * Return: Pointeur vers la chaine modifiée.
+ * Return: Pointeur vers la chaîne modifiée.
  */
 char *string_toupper(char *str)
 {
