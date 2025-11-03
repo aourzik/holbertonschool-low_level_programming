128f8a771fed5ad3d32052122e4b5e93fa4086cf
aourzik <a.ourzik@gmail.com>
2025-11-03 11:02:01 +0100

    feat: create a betty comment for the function

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ *_memcpy - copie n octets d'une zone mémoire vers une autre
+ *@dest: pointeur vers la zone mémoire de destination
+ *@src: pointeur vers la zone mémoire source
+ *@n: nombre d'octets à copier
+ *
+ *Description: Parcourt les n premiers octets de la mémoire pointée
+ *par src et les copie dans la mémoire pointée par dest.
+ *
+ *Return: retourne le pointeur dest (début de la zone mémoire de destination)
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
