0b1764b3bd431b0b419c2cedfe7823b7c4756bd4
aourzik <a.ourzik@gmail.com>
2025-11-03 11:54:22 +0100

    feat: create function that locates a character in a string and return string begin b this character

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strchr - renvoie un pointeur vers le premier caractère trouvé
+ *dans une chaîne
+ *@s: pointeur vers la chaîne de caractères
+ *@c: caractère à chercher
+ *
+ *Description: Parcourt la chaîne pointée par s à la recherche du
+ *caractère c. Si le caractère est trouvé, renvoie un pointeur
+ *vers sa position dans la chaîne. Sinon, renvoie un pointeur
+ *vers le caractère nul terminal.
+ *
+ *Return: pointeur vers le caractère trouvé ou '\0' si non trouvé
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+	}
+	return (&s[i]);
+}
