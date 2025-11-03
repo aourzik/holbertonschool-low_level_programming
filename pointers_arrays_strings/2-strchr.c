1f34e26477dd2c4a737c43f909813c1fc89acbb9
aourzik <a.ourzik@gmail.com>
2025-11-03 11:58:13 +0100

    fix: modify function and condition to control all situations

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -21,6 +21,13 @@ char *_strchr(char *s, char c)
 	while (s[i] != c && s[i] != '\0')
 	{
 		i++;
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else
+		{
+			return (NULL);
+		}
 	}
-	return (&s[i]);
 }
