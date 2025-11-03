1a6c42980511dd6c2e282e64df4ef6070c9160c7
aourzik <a.ourzik@gmail.com>
2025-11-03 10:46:00 +0100

    fix: modify comments to be betty friendly

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * _memset - remplit une zone mémoire avec un octet constant
- * @s: pointeur vers la zone mémoire à remplir
- * @b: valeur de l’octet à répéter
- * @n: nombre d’octets à remplir
+ *_memset - remplit une zone mémoire avec un octet constant
+ *@s: pointeur vers la zone mémoire à remplir
+ *@b: valeur de l’octet à répéter
+ *@n: nombre d’octets à remplir
  *
- * Description: Parcourt les n premiers octets de la mémoire pointée
- * par s et les remplit avec la valeur b.
+ *Description: Parcourt les n premiers octets de la mémoire pointée
+ *par s et les remplit avec la valeur b.
  *
- * Return: retourne le pointeur s (début de la zone mémoire)
+ *Return: retourne le pointeur s (début de la zone mémoire)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
