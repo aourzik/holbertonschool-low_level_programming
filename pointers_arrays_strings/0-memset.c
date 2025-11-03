8c55f1c9fb2538125be9352ff0389a2cdc746d31
aourzik <a.ourzik@gmail.com>
2025-11-03 10:44:33 +0100

    feat: create function that fills memory with a constant byte.

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - remplit une zone mémoire avec un octet constant
+ * @s: pointeur vers la zone mémoire à remplir
+ * @b: valeur de l’octet à répéter
+ * @n: nombre d’octets à remplir
+ *
+ * Description: Parcourt les n premiers octets de la mémoire pointée
+ * par s et les remplit avec la valeur b.
+ *
+ * Return: retourne le pointeur s (début de la zone mémoire)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i = 0;
+
+    while (i < n)
+    {
+        s[i] = b;
+        i++;
+    }
+    return (s);
+}
