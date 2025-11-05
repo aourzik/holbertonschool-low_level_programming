8a7cc051c7499c2bfca45f3fe08d032200978c2d
aourzik <a.ourzik@gmail.com>
2025-11-05 17:49:24 +0100

    feat: create function that adds positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers passed as command-line arguments
+ * @argc: number of arguments passed
+ * @argv: array of strings containing the arguments
+ *
+ * Description:
+ * If no number is passed, prints 0 followed by a new line.
+ * If any argument contains non-digit characters, prints "Error" and returns 1.
+ * Otherwise, sums all positive numbers and prints the result followed by a new line.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
