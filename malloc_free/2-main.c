413a0933915abe4d0da45603e1c44f18a7952429
aourzik <a.ourzik@gmail.com>
2025-11-10 15:41:00 +0100

    fix: modify content of the two files because it wasn't good place

--- malloc_free/2-main.c
+++ malloc_free/2-main.c
@@ -1,50 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * str_concat - Concatène deux chaînes de caractères
- * @s1: première chaîne
- * @s2: seconde chaîne
+ * main - check the code
  *
- * Return: un pointeur vers la nouvelle chaîne concaténée,
- *         ou NULL si l’allocation échoue
+ * Return: Always 0.
  */
-char *str_concat(char *s1, char *s2)
+int main(void)
 {
-	unsigned int i, j, size1, size2;
 	char *s;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-		;
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-		;
-
-	s = malloc((size1 + size2 + 1) * sizeof(char));
-
+	s = str_concat("Best ", "School");
 	if (s == NULL)
 	{
-		return (NULL);
+		printf("failed\n");
+		return (1);
 	}
-
-	for (i = 0; i < size1; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; j < size2; j++)
-	{
-		s[i + j] = s2[j];
-	}
-
-	s[i + j] = '\0';
-
-	return (s);
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,23 +1,50 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - check the code
+ * str_concat - Concatène deux chaînes de caractères
+ * @s1: première chaîne
+ * @s2: seconde chaîne
  *
- * Return: Always 0.
+ * Return: un pointeur vers la nouvelle chaîne concaténée,
+ *         ou NULL si l’allocation échoue
  */
-int main(void)
+char *str_concat(char *s1, char *s2)
 {
+	unsigned int i, j, size1, size2;
 	char *s;
 
-	s = str_concat("Best ", "School");
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+		;
+
+	s = malloc((size1 + size2 + 1) * sizeof(char));
+
 	if (s == NULL)
 	{
-		printf("failed\n");
-		return (1);
+		return (NULL);
 	}
-	printf("%s\n", s);
-	free(s);
-	return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (j = 0; j < size2; j++)
+	{
+		s[i + j] = s2[j];
+	}
+
+	s[i + j] = '\0';
+
+	return (s);
 }
