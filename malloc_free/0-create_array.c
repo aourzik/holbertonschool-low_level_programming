c20fc5ee9c1728994424e34b2703be6ceb43d07b
aourzik <a.ourzik@gmail.com>
2025-11-10 14:29:41 +0100

    fix: modify some points in function to be betty friendly

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -22,14 +22,14 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	array = (char *)malloc(size * sizeof(char));
 
 	if (array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
@@ -37,5 +37,5 @@ char *create_array(unsigned int size, char c)
 		array[i] = c;
 	}
 
-	return array;
+	return (array);
 }
