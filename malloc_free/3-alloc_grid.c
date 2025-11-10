483d2c03909aa6d56260b2fc5b7dfbb15e5cf76d
aourzik <a.ourzik@gmail.com>
2025-11-10 16:47:14 +0100

    fix: modify a variable to be betty friendly

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -32,9 +32,9 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc((width) * sizeof(int));
+		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -44,7 +44,7 @@ int **alloc_grid(int width, int height)
 			free(array);
 			return (NULL);
 		}
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
