797e18029a4bd6cccba4594cd8e4b7766936a198
aourzik <a.ourzik@gmail.com>
2025-11-10 14:27:13 +0100

    fix: add betty comment

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - Crée un tableau de caractères et l'initialise avec un caractère spécifique
+ * @size: la taille du tableau à créer
+ * @c: le caractère utilisé pour initialiser le tableau
+ *
+ * Description:
+ * Cette fonction alloue dynamiquement un tableau de taille 'size',
+ * puis remplit chaque élément avec le caractère 'c'. Si la taille
+ * est nulle ou si l'allocation échoue, la fonction renvoie NULL.
+ *
+ * Return: un pointeur vers le tableau nouvellement créé,
+ *         ou NULL en cas d'erreur ou si size vaut 0.
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *array;
