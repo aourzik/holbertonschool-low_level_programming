7fb8d3541b67a7880d2958835cddba160cb6c8cc
aourzik <a.ourzik@gmail.com>
2025-11-12 10:59:41 +0100

    fix: add betty comment in function

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - alloue de la mémoire et quitte
+ * le programme en cas d’échec
+ * @b: taille en octets à allouer
+ *
+ * Return: pointeur vers la mémoire allouée
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *v;
