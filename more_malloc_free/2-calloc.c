3b1d7a0bd23c02cac9f5b8f72da2d9101ab7a632
aourzik <a.ourzik@gmail.com>
2025-11-12 14:11:58 +0100

    fix: modify function to allocated memory is set to 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,6 +16,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -27,6 +28,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		array[i] = 0;
 	}
 
 	return (array);
