da9df85d08add51cdeb8d706b80080368f1500ee
aourzik <a.ourzik@gmail.com>
2025-11-14 16:40:51 +0100

    fix: modify function to be betty friendly

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,52 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
 /**
- * new_dog - Crée un nouveau chien et copie ses informations
- * @name: nom @age: âge  @owner: propriétaire
- * Return: pointeur vers le dog_t créé, ou NULL si échec
+ * copy_string - Alloue et copie une chaîne de caractères
+ * @str: chaîne à copier
+ *
+ * Return: pointeur vers la nouvelle chaîne, ou NULL si échec
+ */
+char *copy_string(char *str)
+{
+	char *copy;
+	int i;
+	if (!str)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	copy = malloc(i + 1);
+	if (!copy)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+	return (copy);
+}
+/**
+ * new_dog - Crée un nouveau chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Return: pointeur vers le nouveau dog_t, ou NULL si échec
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	if (name)
+
+	d->name = copy_string(name);
+	d->owner = copy_string(owner);
+	if ((name && !d->name) || (owner && !d->owner))
 	{
-		d->name = malloc(strlen(name) + 1);
-		if (!d->name)
-		{
-			free(d);
-			return (NULL);
-		}
-		strcpy(d->name, name);
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
-	else
-		d->name = (NULL);
-	if (owner)
-	{
-		d->owner = malloc(strlen(owner) + 1);
-		if (!d->owner)
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
-		strcpy(d->owner, owner);
-	}
-	else
-		d->owner = (NULL);
+
 	d->age = age;
 	return (d);
 }
