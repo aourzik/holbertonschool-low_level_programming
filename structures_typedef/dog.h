f30885a6f64d9eb1eb7446f7d6175c9e0fa74659
aourzik <a.ourzik@gmail.com>
2025-11-14 12:47:43 +0100

    fix: modify comment ti be betty friendly

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,23 +10,13 @@
  * Description: Cette structure contient les informations de base
  * permettant de décrire un chien.
  */
-
-/**
- * init_dog - Initialise une structure de type dog
- * @d: pointeur vers la structure à initialiser
- * @name: nom du chien
- * @age: âge du chien
- * @owner: propriétaire du chien
- *
- * Description: Initialise les champs de la structure dog pointée
- * par @d avec les valeurs passées en paramètres.
- */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/*Prorotype de la fonction init_dog*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
