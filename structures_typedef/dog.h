e9d33328ccef70a8d16a28977811144701c8755e
aourzik <a.ourzik@gmail.com>
2025-11-14 12:45:33 +0100

    fix: add comm in my library file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure représentant un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: nom du propriétaire du chien
+ *
+ * Description: Cette structure contient les informations de base
+ * permettant de décrire un chien.
+ */
+
+/**
+ * init_dog - Initialise une structure de type dog
+ * @d: pointeur vers la structure à initialiser
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Description: Initialise les champs de la structure dog pointée
+ * par @d avec les valeurs passées en paramètres.
+ */
 struct dog
 {
 	char *name;
