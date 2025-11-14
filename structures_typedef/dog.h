fb28be634bb9fe58f5d0983dde32731a939f9fc0
aourzik <a.ourzik@gmail.com>
2025-11-14 12:17:42 +0100

    feat: create a new type structure with 3 elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure représentant un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Description: Ce fichier définit la structure dog utilisée
+ * dans le projet. Conforme au style Betty.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
