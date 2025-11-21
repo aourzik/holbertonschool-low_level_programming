4f3e79e9e49196f9ff0360a76f545f9e48ab094c
aourzik <a.ourzik@gmail.com>
2025-11-21 15:04:40 +0100

    fix: modify function erasing few if

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,4 +1,3 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -11,34 +10,33 @@
 void print_all(const char *const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *str, *sep = "";
+	unsigned int i = 0;
+	char *sep = "";
+	char *str;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		if (format[i] == 'c' || format[i] == 'i' ||
+			format[i] == 'f' || format[i] == 's')
 		{
-			printf("%s%c", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-			sep = ", ";
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", sep, (double)va_arg(args, double));
-			sep = ", ";
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s%s", sep, str);
+			printf("%s", sep);
+
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			if (format[i] == 'i')
+				printf("%d", va_arg(args, int));
+			if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+			}
+
 			sep = ", ";
 		}
 		i++;
